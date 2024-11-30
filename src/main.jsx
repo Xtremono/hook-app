b3e650232b6d36b2f5c2ef93946de2bc880cce5e
jose tomas ruiz <j_tomas_93@hotmail.com>
2024-11-30 13:06:34 -0300

    CounterApp file created with useState

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { HooksApp } from './HooksApp'
+import { CounterApp } from './01-useState/CounterApp'
+//import { HooksApp } from './HooksApp'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HooksApp />
+    <CounterApp />
   </StrictMode>,
 )
