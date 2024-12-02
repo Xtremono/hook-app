1de9d5b1be1124e20d6dc1fda1c2030002b53d42
jose tomas ruiz <j_tomas_93@hotmail.com>
2024-12-02 12:29:41 -0300

    creacion de custom hook

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
+import { SimpleForm } from './02-useEffect/SimpleForm'
+//import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 //import { CounterApp } from './01-useState/CounterApp'
 //import { HooksApp } from './HooksApp'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <CounterWithCustomHook/>
+    <SimpleForm />
   </StrictMode>,
 )
