b4f8d2044d42b7db8716ff9f7cbbb9c510089d89
jose tomas ruiz <j_tomas_93@hotmail.com>
2024-12-05 13:30:39 -0300

    UseEffect

--- src/02-useEffect/Message.jsx
+++ src/02-useEffect/Message.jsx
@@ -1,12 +1,21 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 export const Message = () => {
 
+
+  const [coords, setCoords] = useState({x:0, y:0});
+
   useEffect(() => {
-    console.log('Message Mounted');
+
+    const onMouseMove = ({x,y}) => {
+      //const coords = {x,y};
+      setCoords({ x, y });
+    }
+
+    window.addEventListener('mousemove', onMouseMove );
 
     return () => {
-      console.log('Message Unmounted');
+      window.removeEventListener('mousemove', onMouseMove );
     }
   }, [])
 
@@ -14,6 +23,7 @@ export const Message = () => {
   return (
     <>
     <h3>Usuario ya existe</h3>
+    { JSON.stringify( coords ) }
     </>
   )
 }
