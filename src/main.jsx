bad05ce304a14ce884b225799252d12f2842ff06
jose tomas ruiz <j_tomas_93@hotmail.com>
2024-12-05 19:33:45 -0300

    UseEffect

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { SimpleForm } from './02-useEffect/SimpleForm'
+//import { SimpleForm } from './02-useEffect/SimpleForm'
+import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
 //import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 //import { CounterApp } from './01-useState/CounterApp'
 //import { HooksApp } from './HooksApp'
 
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <SimpleForm />
-  </StrictMode>,
+  //<StrictMode>
+    <FormWithCustomHook/>
+  //</StrictMode>,
 )
