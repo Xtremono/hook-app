375c3e227c807307e6089b930e5c6f0c49a82686
jose tomas ruiz <j_tomas_93@hotmail.com>
2024-12-10 18:07:17 -0300

    useRef

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,8 @@ import './index.css'
 //import { SimpleForm } from './02-useEffect/SimpleForm'
 //import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
 //import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
-import { FocusScreen } from './04-useRef/FocusScreen'
+//import { FocusScreen } from './04-useRef/FocusScreen'
+import { Memorize } from './05-memos/Memorize'
 //import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 //import { CounterApp } from './01-useState/CounterApp'
 //import { HooksApp } from './HooksApp'
@@ -13,6 +14,6 @@ import { FocusScreen } from './04-useRef/FocusScreen'
 createRoot(document.getElementById('root')).render(
   //<StrictMode>
     //<FormWithCustomHook/>
-    <FocusScreen/>
+    <Memorize/>
   //</StrictMode>,
 )
