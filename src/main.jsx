58e6557a6e410bde96c7f82a717e68db07c22b8b
jose tomas ruiz <j_tomas_93@hotmail.com>
2024-12-20 19:05:38 -0300

    Instalacion de router en reactapp

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from "react-router-dom";
 import './index.css'
+
 //import { SimpleForm } from './02-useEffect/SimpleForm'
 //import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
 //import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
@@ -13,11 +15,13 @@ import './index.css'
 //import { CounterApp } from './01-useState/CounterApp'
 //import { HooksApp } from './HooksApp'
 //import './07-useReducer/intro-reducer';
-import { TodoApp } from './07-useReducer/TodoApp';
+//import { TodoApp } from './07-useReducer/TodoApp';
+import { MainApp } from './08-useContext/MainApp';
 
 createRoot(document.getElementById('root')).render(
-  //<StrictMode>
-    //<FormWithCustomHook/>
-  <TodoApp/>
-  //</StrictMode>,
+  <BrowserRouter>
+  {/*<StrictMode>*/}
+  <MainApp/>
+  {/*</StrictMode>*/}
+  </BrowserRouter>
 )
