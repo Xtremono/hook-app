fb7544074bc1ac954096756b42ee1c4c01970049
jose tomas ruiz <j_tomas_93@hotmail.com>
2024-12-24 16:27:50 -0300

    useContext

--- src/08-useContext/context/UserProvider.jsx
+++ src/08-useContext/context/UserProvider.jsx
@@ -1,15 +1,19 @@
+import { useState } from "react"
 import { UserContext } from "./UserContext"
 
-const user = {
-  id: 123,
-  name: 'Tomas Ruiz',
-  email: 'tomas@gmail.com',
-}
+//const user = {
+  //id: 123,
+  //name: 'Tomas Ruiz',
+  //email: 'tomas@gmail.com',
+//}
 
 export const UserProvider = ({ children }) => {
+
+  const [user, setUser] = useState();
+
   return (
-    <UserContext.Provider value={{ hola:'Mundo', user: user }}>
-      {children }
+    <UserContext.Provider value={{ user, setUser }}>
+      { children }
     </UserContext.Provider>
   )
 }
