fb09b6bc428b4560e0b8d477fad740267839ce4c
jose tomas ruiz <j_tomas_93@hotmail.com>
2024-12-24 12:21:10 -0300

    routes link

---
+++ src/08-useContext/Navbar.jsx
@@ -0,0 +1,14 @@
+import {Link} from "react-router-dom";
+
+
+
+export const Navbar = () => {
+  return (
+    <>
+    <Link to="/">Home</Link>
+    <Link to="/about">About</Link>
+    <Link to="/login">Login</Link>
+    </>
+
+  )
+}
